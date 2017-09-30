dd0391448c9580c5b10f564fa091ff1095fff0a2
Lucas Brilhante <lbrilhante82@gmail.com>
2017-09-30 16:54:26 -0300

    Creating new screen for choosing shape, fixing routes and css.

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,9 @@ Rails.application.routes.draw do
   get 'windings/new' => 'windings#new' , as: 'new_winding'
   post 'windings/create' => 'windings#create' , as: 'create_windings'
   get 'windings/' => 'windings#index' , as: 'windings'
-  get 'windings/:id' => 'windings#show' , as: 'show_winding'
-#  get 'windings/edit/:id' => 'windings#edit', as: 'edit_winding'
-#  patch 'windings/:id/edit', controller: 'windings', action: 'update', as: 'update_winding'
+  get 'windings/show/:id' => 'windings#show' , as: 'show_winding'
   get 'windings/destroy/:id', controller: 'windings', action: 'destroy', as: 'destroy_winding'
+  get 'windings/choose', controller: 'windings', action: 'choose' , as: 'choose_winding'
 
   get 'sessions/index' => 'sessions#index' , as: 'index_sessions'
 
