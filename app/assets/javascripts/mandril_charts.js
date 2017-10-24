7a7597200416405e0a44899e39039c9293a07b04
Jessica Cristina <jessica.cris1127@gmail.com>
2017-10-23 22:13:53 -0200

    Create graph page

--- app/assets/javascripts/mandril_charts.js
+++ app/assets/javascripts/mandril_charts.js
@@ -1,50 +1,41 @@
-$(document).ready(function() {
-
-function cylinder(r, h) {
-  var at = 2*(3.14*r**2) + 2*(3.14*r*h);
-  return at;
-}
-
-function plotGraph(radius, length) {
-
-  var ctx = document.getElementById("mandril").getContext('2d');
-
-  if (radius === '' || length === '') {
-    var myLineChart = new Chart(ctx, {});
-    myLineChart.destroy();
-    return;
+function ready(){
+  function cylinder(r, h) {
+    var at = 2*(3.14*r**2) + 2*(3.14*r*h);
+    return at;
   }
 
+  function plotGraph(radius, length) {
 
-  var area = cylinder(radius, length);
+    var ctx = document.getElementById("mandril").getContext('2d');
 
-  var myLineData = {
-    labels: ["","Area"],
-    datasets: [{
-      label: "Arroz",
-      backgroundColor: "rgba(0,255,0,1)",
-      data: [area, area]
-    }]
+    if (radius === '' || length === '') {
+      var myLineChart = new Chart(ctx, {});
+      myLineChart.destroy();
+      return;
+    }
+
+    var area = cylinder(radius, length);
+
+    var myLineData = {
+      labels: ["","Area"],
+      datasets: [{
+        label: "Arroz",
+        backgroundColor: "rgba(0,255,0,1)",
+        data: [area, area]
+      }]
+    }
+
+    var myLineChart = new Chart(ctx, {
+        type: 'line',
+        data: myLineData,
+    });
   }
-
-  var myLineChart = new Chart(ctx, {
-      type: 'line',
-      data: myLineData,
-  });
+  var graphContainer = document.getElementById("graph-container")
+  if (graphContainer != null){
+    var length = document.getElementById("length").dataset.length
+    var radius = document.getElementById("radius").dataset.radius
+    plotGraph(radius, length)
+  }
 }
 
-var lengthInput = document.getElementById("lengthInput");
-var radiusInput = document.getElementById("radiusInput");
-
-
-lengthInput.addEventListener("input", function(){
-  var radius = radiusInput.value;
-  plotGraph(radius, this.value);
-});
-
-radiusInput.addEventListener("input", function() {
-  var length = lengthInput.value;
-  plotGraph(this.value, length);
-});
-
-})
+$(document).on("turbolinks:load", ready)
