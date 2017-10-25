cf88e606f99ee285b50c1e62d5b95cba1c62b0db
Pedro Sales <pedrohsalesf@gmail.com>
2017-10-24 22:07:57 -0200

    Implementing controller search mechanism

--- app/controllers/windings_controller.rb
+++ app/controllers/windings_controller.rb
@@ -6,6 +6,11 @@ class WindingsController < ApplicationController
   # GET /windings.json
   def index
     @windings = Winding.all
+    if params[:search]
+      @windings = Winding.search(params[:search]).order("created_at DESC")
+    else 
+      @windings = Winding.all.order('created_at DESC')
+    end
   end
 
   # GET /windings/1
@@ -29,6 +34,7 @@ class WindingsController < ApplicationController
 
 
     if @winding.filamentLength > needed_wire
+      @winding.windingdate = DateTime.now.to_date
       if @winding.save
         sendgcode
         redirect_to :action => "monitor"
