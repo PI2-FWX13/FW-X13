414c7eaf91b5ea2dd8902e33b43b045d66b1bc3b
Jessica Cristina <jessica.cris1127@gmail.com>
2017-11-22 12:18:13 -0200

    Tested create of MachineSettinhsController

--- spec/controllers/machine_settings_controller_spec.rb
+++ spec/controllers/machine_settings_controller_spec.rb
@@ -13,5 +13,32 @@ RSpec.describe MachineSettingsController, type: :controller do
 
       expect(response).to render_template("index")
     end
+
+    it "create new machine setting" do
+      ms = MachineSetting.all
+
+      get :index
+
+      expect(assigns(:machine_settings)).to eq(ms)
+
+    end
   end
+
+  describe "POST #create" do
+
+    it "It will create a new machine settings" do
+      post :create, params: {:machine_setting => {:offset=> "101", :maximum_size_mandril=> "81", :size_radius=> "66"}}
+
+      expect(response).to have_http_status(302)
+
+    end
+
+    it "It not will create a new machine setting" do
+      post :create, params: {:machine_setting => {:offset=> "", :maximum_size_mandril=> "", :size_radius=> ""}}
+
+      expect(response).to have_http_status(302)
+
+    end
+  end
+
 end
