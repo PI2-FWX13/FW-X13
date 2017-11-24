487c026d0d05ab3cd1a59b1c5508dab5ca59ca35
Joao Paulo Busche <joaopaulo.busche@gmail.com>
2017-11-24 15:25:07 -0200

    Making current connection a global variable

--- app/helpers/conection_informations_helper.rb
+++ app/helpers/conection_informations_helper.rb
@@ -1,5 +1,5 @@
 module ConectionInformationsHelper
 	def current_connection(connection)
-		@current_connection = connection
+		$current_connection = ConectionInformation.find(connection.id)
 	end
 end
