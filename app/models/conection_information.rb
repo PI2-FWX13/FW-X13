d0c4cdbda735e086bc74d3d436d3d2582f3ee583
Pedro Sales <pedrohsalesf@gmail.com>
2017-11-23 22:27:51 -0200

    Init implementation of wifi connnection

--- app/models/conection_information.rb
+++ app/models/conection_information.rb
@@ -1,4 +1,6 @@
 class ConectionInformation < ApplicationRecord
 	validates :name, presence: true, uniqueness: true
 	validates :ip, presence: true, uniqueness: true
+	validates :wifi_name, presence: false, uniqueness: false
+	validates :password, presence: false, uniqueness: false
 end
